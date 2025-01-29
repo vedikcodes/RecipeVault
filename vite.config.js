61c69a0d79f62d1fbac9acec6ab427f2c24aa787
Vedik Tiwari <165828663+vedikcodes@users.noreply.github.com>
2025-01-29 12:54:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
-import tailwind from "tailwindcss"
+import tailwind from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   css:{
-    postcss:[tailwind]
-   }
+  css: {
+    postcss: [tailwind],
+  },
+  server: {
+    host: '0.0.0.0', // Ensures that the server listens on all network interfaces
+    port: process.env.PORT || 5000, // Use the port provided by Render (or fallback to 5000 locally)
+  },
 })
